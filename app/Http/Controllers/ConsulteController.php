edf672453a4d17d7ca8a354683fb4646c9909932
Aminata Mbodj <35398092+aminaousmane@users.noreply.github.com>
2019-01-02 12:53:12 +0000

    Update ConsulteController.php

--- app/Http/Controllers/ConsulteController.php
+++ app/Http/Controllers/ConsulteController.php
@@ -7,26 +7,7 @@ use App\Consulte;
 use DB;
 class ConsulteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
- 
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         return view('consultation');
@@ -36,6 +17,20 @@ class ConsulteController extends Controller
     {
         $consulte = DB::table('consulte')->selectRaw('consulte.id_malade,consulte.id_medecins,consulte.obsevation_consult,consulte.frais_consultation,consulte.date_consultation')->count();
             return view('home',compact('consulte'));
+    }
+ 
+    public function store(Request $request)
+    {
+        $data = ['id_malade'=>$request->malade,
+                 'id_medecins'=>$request->medecins,
+                 'obsevation_consult'=>$request->observation,
+                 'frais_consultation'=>$request->frais,
+                 'date_consultation'=>$request->date,
+                ];
+                DB::table('consulte')->insert($data);
+        
+        
+        return back()->with('msg', 'Ajout avec succes');
     }
 
     

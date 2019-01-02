b3d496bb671430ae03fd368052f6a149d48fa39b
Aminata Mbodj <35398092+aminaousmane@users.noreply.github.com>
2019-01-02 12:54:03 +0000

    Update maladeController.php

--- app/Http/Controllers/maladeController.php
+++ app/Http/Controllers/maladeController.php
@@ -8,10 +8,17 @@ use DB;
 
 class maladeController extends Controller
 {
-    public function show()
+    public function index()
     {
         return view('malade');
     }
+    public function show(Malade $malade)
+    {
+        $malade = DB::table('malade')->selectRaw('malade.id_malade,malade.nom_malade,malade.prenom_malade')->get();
+        $medecin = DB::table('medecins')->selectRaw('medecins.id_medecins,medecins.nom_medecins,medecins.prenom_medecins')->get();
+        return view('consultation',compact('malade'),compact('medecin'));
+    }
+   
     public function store(Request $request)
     {
         $data = ['nom_malade'=>$request->nom,
@@ -26,7 +33,7 @@ class maladeController extends Controller
         
         return back()->with('msg', 'Ajout avec succes');
     }
-   
+  
        
 
 }

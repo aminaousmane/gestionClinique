484dc877b8196bb2e34b2aaa434035be170a5eb7
Aminata Mbodj <35398092+aminaousmane@users.noreply.github.com>
2019-01-02 12:51:55 +0000

    Update consultation.blade.php

--- resources/views/consultation.blade.php
+++ resources/views/consultation.blade.php
@@ -42,13 +42,17 @@
     <div class="clear"></div>
     <div class="row col-md-10 col-md-offset-1">
 
-      <form id="formID" action="" method="post">
+      <form id="formID" action="{{url('storeConsult')}}" method="post">
+      {{ csrf_field()}} 
           
               <label class="">Nom du patient: <span style="color:red; font-weight: bold; font-family: Arial, sans-serif ;">(*)</span></label>
               <div class="input-group">
                   <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
                   <select name="malade" class="form-control validate[required]">
                   <option selected="selected" value="">Selectionnée</option>
+                  @foreach($malade as $c)
+                     <option value="{{$c->id_malade}}"> {{$c->prenom_malade}}  {{$c->nom_malade}}</option>; 
+                 @endforeach
 
                   </select>
               </div><br>
@@ -57,6 +61,9 @@
                   <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
                   <select name="medecins" class="form-control validate[required]">
                   <option selected="selected" value="">Selectionnée</option>
+                  @foreach($medecin as $c)
+                    <option value="{{$c->id_medecins}}"> {{$c->prenom_medecins}}  {{$c->nom_medecins}}</option>; 
+                  @endforeach
 
                   </select>
               </div><br>
